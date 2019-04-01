1aceeb76cd6a941cd078aa213fba76f82c498cff
DingZhenyuan <ding_zy@outlook.com>
2019-04-01 10:42:49 +0800

    Add extra part.

--- svhn_tyrecord.py
+++ svhn_tyrecord.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from scipy.io import loadmat
 
 
+# 从路径中提取image和label
 def data_set(name, num_sample_size=10000):
     if name == 'train':
         filename = "svhn_data/format2/train_32x32.mat"
@@ -20,20 +21,23 @@ def data_set(name, num_sample_size=10000):
 
     image = image[:num_sample_size]
     label = label[:num_sample_size]
-    return train_x, train_y
+    return image, label
 
 
+# 封装数据格式
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
+# 封装数据格式
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def convert_to_tfrecords(images, labels, fileName):
     num_examples, rows, cols, depth = images.shape
-  
+
+    # 写入过程
     print('Writing', fileName)
     writer = tf.python_io.TFRecordWriter(fileName)
     for index in range(num_examples):
@@ -48,6 +52,7 @@ def convert_to_tfrecords(images, labels, fileName):
     writer.close()
 
 
+# 拆分数据集
 def split_dataset(train_x, train_y, validation_size):
     return (train_x[:-validation_size],
             train_y[:-validation_size],
@@ -56,18 +61,19 @@ def split_dataset(train_x, train_y, validation_size):
 
 
 with tf.Session() as sess:
-    train_x, train_y = data_set('train')
-    test_x, test_y = data_set('test')
-    train_x, train_y, valid_x, valid_y = split_dataset(train_x, train_y, 1000)
+    train_x, train_y = data_set('train', 30000)
+    test_x, test_y = data_set('test', 10000)
+    # extra_x, extra_y = data_set('extra', 10000)
+
+    # 需要分割获得validation时用
+    # train_x, train_y, valid_x, valid_y = split_dataset(train_x, train_y, 1000)
 
     trainFileName = "svhn_data/format2/train.tfrecords"
-    # validationFileName = "svhn_data/format2/validation.tfrecords"
     testFileName = "svhn_data/format2/test.tfrecords"
     extraFileName = "svhn_data/format2/extra.tfrecords"
 
     # Convert to Examples and write the result to TFRecords.
-    # convert_to_tfrecords(train_x, train_y, trainFileName, num_sample_size=20000)
     convert_to_tfrecords(train_x, train_y, trainFileName)
-    convert_to_tfrecords(test_x, test_y, extraFileName)
-    convert_to_tfrecords(valid_x, valid_y, testFileName)
-    print('over')
+    convert_to_tfrecords(test_x, test_y, testFileName)
+    # convert_to_tfrecords(extra_x, extra_y, extraFileName)
+    print('Over')
